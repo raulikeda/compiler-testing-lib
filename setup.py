8e16b1c557f11d8c7c5525123fbe7f1b9af6a647
Raul Ikeda <rauligs@insper.edu.br>
2025-07-17 17:34:44 -0300

    lib description

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ import os
 # Ensure the build directory exists
 os.makedirs('build', exist_ok=True)
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='compiler-testing-lib',
-    version='0.1.5',
+    version='0.1.6',
     description='A testing library for running language/compiler tests from YAML metadata.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Your Name',
     packages=find_packages(),
     include_package_data=True,

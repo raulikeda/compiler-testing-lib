2b864802c8edd715cb45461997c2b215350f680d
Raul Ikeda <rauligs@insper.edu.br>
2025-07-17 17:31:10 -0300

    new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.makedirs('build', exist_ok=True)
 
 setup(
     name='compiler-testing-lib',
-    version='0.1.4',
+    version='0.1.5',
     description='A testing library for running language/compiler tests from YAML metadata.',
     author='Your Name',
     packages=find_packages(),

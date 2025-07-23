46b5b6799bda3e0528429c36d2bca2368dd71559
Raul Ikeda <rauligs@insper.edu.br>
2025-07-23 17:55:14 -0300

    args adjustments

--- compiler_testing_lib/runner.py
+++ compiler_testing_lib/runner.py
@@ -27,6 +27,10 @@ class TestRunner:
         for idx, test in enumerate(self.tests):
             test_file = os.path.join(self.base_path, f"{test['name']}.{self.file_extension}")
             command = f"{command_template} {test_file}"
+            if self.version in ['v0.0', 'v1.0', 'v1.1', 'v2.0']:
+                with open(test_file, 'r') as f:
+                    code = f.read()
+                command = f"{command_template} '{code}'"
             try:
                 input_values = ('\n').join(test['input'])
                 result = subprocess.run(command, shell=True, capture_output=True, text=True, timeout=self.timeout, input=input_values)
